c0ad0b6c8da1d2f914d5c0dd9a877291d29f190b
fenstergucker <86307310+fenstergucker@users.noreply.github.com>
2022-01-03 19:17:16 +0100

    added samples

--- sample3.js
+++ sample3.js
@@ -1,13 +1,2 @@
-class Animal {
-	constructor(name) {
-		this.name = name
-	}
 
-	makeSound() {
-		console.log("I make some noise!")
-	}
 
-}
-
-const donald = new Animal("donlad");
-donald.makeSound();
